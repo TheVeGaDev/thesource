b6bcbceb46ae5a4dc655ec564ef0ebdab9ee88a1
TheVeGaDev <m.medhat2610@gmail.com>
2025-09-16 02:52:36 +0300

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -31,7 +31,7 @@ class Config(object):
     TZ = os.environ.get("TZ", "Asia/Baghdad")
     # set this with required zed repo link
     UPSTREAM_REPO = os.environ.get(
-        "UPSTREAM_REPO", "https://github.com/Tepthonee/thesource"
+        "UPSTREAM_REPO", "https://github.com/TheVeGaDev/thesource"
     )
     # External plugins repo
     EXTERNAL_REPO = os.environ.get("EXTERNAL_REPO", None)
@@ -187,9 +187,9 @@ class Config(object):
     BOTLOG_CHATID = 0
     # extra plugins realted vars below  4
     EXTERNAL_REPOBRANCH = os.environ.get("EXTERNAL_REPOBRANCH", "main")
-    OLDZED_REPO = os.environ.get("OLDZED_REPO", "https://github.com/Tepthonee/thesource")
+    OLDZED_REPO = os.environ.get("OLDZED_REPO", "https://github.com/TheVeGaDev/thesource")
     if OLDZED_REPO and not url(OLDZED_REPO):
-        OLDZED_REPO = "https://github.com/Tepthonee/thesource"
+        OLDZED_REPO = "https://github.com/TheVeGaDev/thesource"
     OLDZED_REPOBRANCH = os.environ.get("OLDZED_REPOBRANCH", "oldzed")
     VC_REPO = os.environ.get("VC_REPO", "https://github.com/Zed-Thon/ZVCPlayer")
     VC_REPOBRANCH = os.environ.get("VC_REPOBRANCH", "zvcplayer")
