8d8094a38aec3e508757400405676b88d741a977
TheVeGaDev <m.medhat2610@gmail.com>
2025-09-16 21:26:20 +0300

    Update startup.py

--- Tepthon/utils/startup.py
+++ Tepthon/utils/startup.py
@@ -273,38 +273,27 @@ async def load_plugins(folder, extfolder=None):
             path1 = Path(f.name)
             shortname = path1.stem
             pluginname = shortname.replace(".py", "")
-            try:
-                if (pluginname not in Config.NO_LOAD) and (
-                    pluginname not in VPS_NOLOAD
-                ):
-                    flag = True
-                    check = 0
-                    while flag:
-                        try:
-                            load_module(
-                                pluginname,
-                                plugin_path=plugin_path,
-                            )
-                            if shortname in failure:
-                                failure.remove(shortname)
-                            success += 1
-                            break
-                        except ModuleNotFoundError as e:
-                            install_pip(e.name)
-                            check += 1
-                            if shortname not in failure:
-                                failure.append(shortname)
-                            if check > 5:
-                                break
+            flag = True
+            check = 0
+            while flag:
+                try:
+                    load_module(
+                        pluginname,
+                        plugin_path=plugin_path,
+                    )
+                    if shortname in failure:
+                        failure.remove(shortname)
+                    success += 1
+                    break
+                except ModuleNotFoundError as e:
+                    install_pip(e.name)
+                    check += 1
+                    if shortname not in failure:
+                        failure.append(shortname)
+                    if check > 5:
+                        break
                 else:
-                    os.remove(Path(f"{plugin_path}/{shortname}.py"))
-            except Exception as e:
-                if shortname not in failure:
-                    failure.append(shortname)
-                os.remove(Path(f"{plugin_path}/{shortname}.py"))
-                LOGS.info(
-                    f"لا يمكنني تحميل {shortname} بسبب الخطأ {e}\nمجلد القاعده {plugin_path}"
-                )
+                    pass
     if extfolder:
         if not failure:
             failure.append("None")
@@ -312,7 +301,6 @@ async def load_plugins(folder, extfolder=None):
             BOTLOG_CHATID,
             f'Your external repo plugins have imported \n**No of imported plugins :** `{success}`\n**Failed plugins to import :** `{", ".join(failure)}`',
         )
-
 
 
 async def verifyLoggerGroup():
